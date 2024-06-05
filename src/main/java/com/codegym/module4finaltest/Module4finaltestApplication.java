018e088fbe119f1f9932469bd32ed7bd78aa6acd
=tdu92 <tdu92>
2024-06-05 19:31:31 +0700

    first commit

---
+++ src/main/java/com/codegym/module4finaltest/Module4finaltestApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.module4finaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4finaltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4finaltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/module4finaltest/Module4finaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.module4finaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4finaltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
